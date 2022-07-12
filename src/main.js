879af94254c9df580cdbc91de5c7245b41819288
WeridDanimal <dz574214@gmail.com>
2022-07-11 19:04:46 -0500

    added table with pagination for customer and user

--- src/main.js
+++ src/main.js
@@ -2,11 +2,17 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import store from "./store";
 import router from "./router";
+import axios from 'axios';
 import "./assets/css/nucleo-icons.css";
 import "./assets/css/nucleo-svg.css";
 import ArgonDashboard from "./argon-dashboard";
 import PrimeVue from 'primevue/config';
-
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import 'primevue/resources/themes/bootstrap4-light-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+import Button from 'primevue/button';
 
 
 const appInstance = createApp(App);
@@ -14,5 +20,9 @@ const appInstance = createApp(App);
 appInstance.use(store);
 appInstance.use(router);
 appInstance.use(ArgonDashboard);
-appInstance.mount("#app");
+appInstance.component('DataTable',DataTable);
+appInstance.component('Column',Column);
 appInstance.use(PrimeVue);
+appInstance.component('Button',Button);
+appInstance.config.globalProperties.axios=axios;
+appInstance.mount("#app");
--- src/views/Customers.vue
+++ src/views/Customers.vue
@@ -1,27 +1,20 @@
 <template>
   <div class="py-4 container-fluid">
-    <div class=" row">
-      <div class="col-12">
-        <authors-table />
-      </div>
-    </div>
     <div class="mt-4 row">
       <div class="col-12">
-        <projects-table />
+        <customer-table />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import AuthorsTable from "./components/AuthorsTable.vue";
-import ProjectsTable from "./components/ProjectsTable.vue";
+import CustomerTable from "./components/CustomerTable.vue";
 
 export default {
   name: "Customers",
   components: {
-    AuthorsTable,
-    ProjectsTable
+    CustomerTable
   },
   data() {
     return {
